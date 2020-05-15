f17f07060536b70edb686e39d4c9119b022e8cff
KC <krkeco@gmail.com>
2020-05-15 14:01:07 -0700

    draft mobile

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { StylingService } from '../styling.service';
+import { faCoffee, faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu',
@@ -8,6 +9,8 @@ import { StylingService } from '../styling.service';
   styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+	faCoffee = faCoffee;
+	faBars = faBars;
   constructor(private stylingService: StylingService) {}
 
   ngOnInit(): void {}
